a8cc4955732cf06c0758e8483ed05021c41470c6
hd724 <asp.net1367@gmail.com>
2020-06-16 12:05:08 +0430

    n

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 
-import {PhotoService} from "./photo.service";
+
 
 
 @Component({
@@ -10,26 +10,11 @@ import {PhotoService} from "./photo.service";
 })
 
 export class HeaderComponent implements OnInit {
-  images: any[];
 
-  responsiveOptions: any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1
-    }
-  ];
-  constructor(private photoService: PhotoService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.photoService.getImages().then(images => this.images = images);
+
   }
 }
 
