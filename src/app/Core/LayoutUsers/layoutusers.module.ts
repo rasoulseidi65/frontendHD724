ef249606c52f7a93878bba43f65ef4fcd583c362
hd724 <asp.net1367@gmail.com>
2020-06-19 15:44:16 +0430

    اصلاحیه قسمت وضعیت

--- src/app/Core/LayoutUsers/layoutusers.module.ts
+++ src/app/Core/LayoutUsers/layoutusers.module.ts
@@ -8,11 +8,15 @@ import {AccordionModule} from 'primeng/accordion';
 import {MenubarModule, ToolbarModule} from 'primeng/primeng';
 import {LayoutModule} from "../../layout/layout.module";
 import { CounteruserComponent } from './counteruser/counteruser.component';
+import { MyProductsComponent } from './Condition/my-products/my-products.component';
+import { MyPurchasesComponent } from './condition/my-purchases/my-purchases.component';
+import { MyCoursesComponent } from './condition/my-courses/my-courses.component';
+import { MySalesComponent } from './condition/my-sales/my-sales.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, CounteruserComponent],
+  declarations: [DashboardComponent, CounteruserComponent, MyProductsComponent, MyPurchasesComponent, MyCoursesComponent, MySalesComponent],
     imports: [
         CommonModule,
         LayoutUsersRoutingModule,
