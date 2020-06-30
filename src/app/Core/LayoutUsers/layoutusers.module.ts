4d6a9ea3d0b163701b73e0b906194e1211b9d47c
hd724 <asp.net1367@gmail.com>
2020-06-30 18:22:17 +0430

    تفییرا جزئی تاریخ10/3/99

--- src/app/Core/LayoutUsers/layoutusers.module.ts
+++ src/app/Core/LayoutUsers/layoutusers.module.ts
@@ -5,18 +5,24 @@ import { LayoutUsersRoutingModule } from './layoutusers-routing.module';
 import {DashboardComponent} from './dashboardUser/dashboard.component';
 import {MatButtonModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {AccordionModule} from 'primeng/accordion';
-import {MenubarModule, ToolbarModule} from 'primeng/primeng';
+import {ButtonModule, MenubarModule, ToolbarModule} from 'primeng/primeng';
 import {LayoutModule} from "../../layout/layout.module";
 import { CounteruserComponent } from './counteruser/counteruser.component';
 import { MyProductsComponent } from './Condition/my-products/my-products.component';
 import { MyPurchasesComponent } from './condition/my-purchases/my-purchases.component';
 import { MyCoursesComponent } from './condition/my-courses/my-courses.component';
 import { MySalesComponent } from './condition/my-sales/my-sales.component';
+import { TransactionsComponent } from './Financial/transactions/transactions.component';
+import { ReportsComponent } from './Financial/reports/reports.component';
+import { SubscriberAffairsComponent } from './system/subscriber-affairs/subscriber-affairs.component';
+import { SupportTicketComponent } from './system/support-ticket/support-ticket.component';
+import { UserAccountComponent } from './Account-Settings/user-account/user-account.component';
+
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, CounteruserComponent, MyProductsComponent, MyPurchasesComponent, MyCoursesComponent, MySalesComponent],
+  declarations: [DashboardComponent, CounteruserComponent, MyProductsComponent, MyPurchasesComponent, MyCoursesComponent, MySalesComponent, TransactionsComponent, ReportsComponent, SubscriberAffairsComponent, SupportTicketComponent, UserAccountComponent],
     imports: [
         CommonModule,
         LayoutUsersRoutingModule,
@@ -27,7 +33,8 @@ import { MySalesComponent } from './condition/my-sales/my-sales.component';
         ToolbarModule,
         MatButtonModule,
         MenubarModule,
-        LayoutModule
+        LayoutModule,
+        ButtonModule
     ]
 })
 export class LayoutusersModule { }
